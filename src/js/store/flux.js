d3288139bb703067c955511a86e2f66a61725363
theliquidbacon <146387957+theliquidbacon@users.noreply.github.com>
2024-04-17 06:11:58 +0000

    Donete

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,109 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
+			showModal: false,
+			contactToBeDeleted: null,
+			contacts: [
 				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
+					id: 1,
+					name: "Nolan Grayson",
+					homeAddress: "2827 Chicago",
+					phone: "(405) 270-0441",
+					email: "omniman@exampleemail.com"
 				},
 				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
+					id: 2,
+					name: "Alex Louis Armstrong",
+					homeAddress: "2387 Hillhaven",
+					phone: "(210) 757-0694",
+					email: "FMA@exampleemail.com"
+				},
+				{
+					id: 3,
+					name: "Ron Swanson",
+					homeAddress: "4907 Colonial Park",
+					phone: "(704) 746-7010",
+					email: "justron@exampleemail.com"
+				},
+				{
+					id: 4,
+					name: "J. Jonah Jameson",
+					homeAddress: "2373 NY",
+					phone: "(562) 309-5061",
+					email: "dailybugle@exampleemail.com"
+				},
 			]
 		},
+
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			//add contact to the list
+			addContact: (contact) => {
+				// pulls contacts from the store
+				let listOfContacts = getStore().contacts;
+				// creates an id for the new contact and add rest of its info
+				const newContact = {
+					id: listOfContacts.length + 1,
+					...contact
+				};
+				// adds new contact to the list
+				setStore({ contacts: [...listOfContacts, newContact] });
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+
+			//edit contact from the list
+			editContact: (id, updatedContact) => {
+				// pulls contacts from the store
+				let listOfContacts = getStore().contacts;
+				// if contact with id exists then update it
+				const contactIndex = listOfContacts.findIndex(contact => contact.id === id);
+				if (contactIndex !== -1) {
+					const updatedContacts = [...listOfContacts];
+					updatedContacts[contactIndex] = { id, ...updatedContact };
+					// updates the contacts' info in the store
+					setStore({ contacts: updatedContacts });
+				}
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+			// toggle the modal on
+			toggleModal: (show) => {
+				setStore({ showModal: show })
+			},
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			// checks if all fields are filled else shows modals
+			checkEmptyFields: (newContact) => {
+				const { name, homeAddress, phone, email } = newContact;
+				if (name && homeAddress && phone && email) {
+					// if all fields are filled, saves contact
+					getActions().addContact(newContact);
+				} else {
+					// if any field is empty, shows modal
+					getActions().toggleModal(true);
+				}
+			},
+
+			//close modal button
+			closeModal: () => {
+				setStore({ showModal: false });
+			},
+
+			// set contact to be deleted
+			setContactToBeDeleted: (contact) => {
+				setStore({ contactToBeDeleted: contact });
+			},
+
+			// close the confirm delete modal without deleting a record
+			closeDeleteModal: () => {
+				setStore({ showModal: false, contactToBeDeleted: null });
+			},
+
+			//delete contact from the list
+			deleteContact: (contact) => {
+				//pulls the contacts in the store
+				let listOfContacts = getStore().contacts;
+				//filters the contact and generates new array
+				setStore({ contacts: listOfContacts.filter((item) => item !== contact) });
+				// close the confirm delete modal after deleting a record
+				getActions().closeDeleteModal();
+			},
 		}
 	};
 };
